36e43866054ba02ecbf4dbde1a78b75e674359c1
Tim Standen <timmstanden@gmail.com>
2023-03-18 14:38:22 -0700

    add security check on editing endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
+    http_basic_authenticate_with( 
+        name: "admin", 
+        password: "starsector2022", 
+        except: [:show, :index]
+    )
 end
