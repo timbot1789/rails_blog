655dfdb8adc4a427e65571e05c88615aa17c81a9
Tim Standen <timmstanden@gmail.com>
2023-03-17 17:46:15 -0700

    change blog ordering

--- app/controllers/intro_controller.rb
+++ app/controllers/intro_controller.rb
@@ -1,7 +1,7 @@
 class IntroController < ApplicationController
 
   def show 
-    @articles = Article.where(status: "OPEN").last(5)
+    @articles = Article.where(status: "OPEN").order(updated_at: :desc).first(5)
   end
 end
   
