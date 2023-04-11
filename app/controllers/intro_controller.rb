7bfacfe22bfe761adf9558f2cc1fdf327466e5e9
Tim Standen <timmstanden@gmail.com>
2023-04-11 15:19:26 -0700

    fix bug with pagination

--- app/controllers/intro_controller.rb
+++ app/controllers/intro_controller.rb
@@ -5,9 +5,8 @@ class IntroController < ApplicationController
   end
 
   def show 
-    params = params ? params : {}
-    requested_page = (params.key?(:page) && is_positive?(params[:page])) ? params[:page].to_i : 1
-    count_per_page = (params.key?(:count_per_page) && is_positive?(params[:count_per_page])) ? params[:count_per_page].to_i : 10
+    requested_page = (params.key?("page") && is_positive?(params["page"])) ? params["page"].to_i : 1
+    count_per_page = (params.key?("count_per_page") && is_positive?(params["count_per_page"])) ? params["count_per_page"].to_i : 10
     article_count = Article.where(status: "OPEN").count
     @num_pages = (article_count.to_f / count_per_page).ceil
     @page = (requested_page > @num_pages) ? @num_pages : requested_page
